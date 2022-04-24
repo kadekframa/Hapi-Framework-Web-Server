c1659154571fc89d2209f02bcea9fe3fc35833e1
kadekframa <dframa19@gmail.com>
2022-04-24 17:08:08 +0800

    Hapi Framework, Path Parameter

--- routes.js
+++ routes.js
@@ -28,11 +28,19 @@ const routes = [
         },
     },
     {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            return `Hello, ${name}!`;
+        }
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
-            return 'Halaman tidak dapat ditemukan';
-        },
+            return 'Halaman tidak ditemukan';
+        }
     },
 ];
 
